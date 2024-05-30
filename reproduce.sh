8ed495d5ef1472d77a4aab4bfda75bf17761b301
Cameron Riddell <cameron@dutc.io>
2024-05-30 14:29:35 -0700

    update reproduce.sh

--- reproduce.sh
+++ reproduce.sh
@@ -1,8 +1,3 @@
-# requirements setup and mamba for faster install for env
-source /opt/conda/etc/profile.d/conda.sh
-mamba env create -qq -f environment.yml
-conda activate krusellsmith
-
 # execute the script to create figures
 cd Code/Python
-ipython KrusellSmith.py
+ipython --classic --matplotlib=agg KrusellSmith.py
